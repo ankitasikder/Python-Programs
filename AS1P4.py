fca81c6a5af4abc77324da6a215c1174a0c24ded
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-10 19:58:32 +0530

    by ankita

--- AS1P4.py
+++ AS1P4.py
@@ -5,4 +5,13 @@ print("Check if "+a_string+" contains "+substring1+":")
 print(substring1 in a_string)
 print("Check if "+a_string+" contains "+substring2+":")
 print(substring2 in a_string)
+'''ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com'''
 
