0d771aa1433e770c39dfd1428d7f05a2cdd5a2dc
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-10 19:59:19 +0530

    by ankita

--- AS1P5.py
+++ AS1P5.py
@@ -1,3 +1,12 @@
 for i in range(2000,3001):
     if i%7==0:
         print(i)
+'''ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com'''
