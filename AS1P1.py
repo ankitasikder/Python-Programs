d3a91d86d6ead53cc36602d68f584c56e89b6668
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-10 19:57:54 +0530

    by ankita

--- AS1P1.py
+++ AS1P1.py
@@ -4,3 +4,12 @@ for i in range(1,n+1):
         print("even:",i)
     else:
         print("odd:",i)
+'''ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com'''
