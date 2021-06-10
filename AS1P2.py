bfb288013742758e51d56b5a753329896b7a184e
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-10 19:58:14 +0530

    by ankita

--- AS1P2.py
+++ AS1P2.py
@@ -4,3 +4,12 @@ def facto(n):
         return 1
     return n*facto(n-1)
 print("factorial of {}: ".format(n),facto(n))
+'''ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com'''
