7774a5ea408a6b6d114dd3281f9275af2c3414e8
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-10 20:00:03 +0530

    by ankita

--- AS1P7.py
+++ AS1P7.py
@@ -5,3 +5,12 @@ for strr in listi:
         print(strr)
     else:
         continue
+        '''ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com'''
