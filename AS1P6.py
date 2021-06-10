a59217b46f276e17af60a3d778edff6475d7fbac
ankitasikder <79798763+ankitasikder@users.noreply.github.com>
2021-06-10 19:59:40 +0530

    by ankita

--- AS1P6.py
+++ AS1P6.py
@@ -3,3 +3,12 @@ if str1==str1[::-1]:
     print("IT IS PALINEDROME STRING")
 else:
     print("NOT PALINDROME")
+    '''ALL THESE WORKS ARE DONE BY 
+
+ANKITA SIKDER
+
+STUDENT OF BTECH, IN UEMK
+
+CONTACT NO.: 8583939774
+
+EMAIL ID: ankita.sikder14@gmail.com'''
